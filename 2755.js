2f53962c7356f382802a35c195c861314c56d473
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:19:40 -0300

    Create 2755.js

---
+++ 2755.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('"Ro\'b\'er\tto\\/"');
+console.log('(._.) ( l: ) ( .-. ) ( :l ) (._.)');
+console.log('(^_-) (-_-) (-_^)');
+console.log('("_") (\'.\')');
