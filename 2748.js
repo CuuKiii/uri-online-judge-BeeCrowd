a116395fdc40bdb13ae091cba5857384a1abde43
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:29:12 -0300

    Create 2748.js

---
+++ 2748.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('---------------------------------------');
+console.log('|        Roberto                      |');
+console.log('|                                     |');
+console.log('|        5786                         |');
+console.log('|                                     |');
+console.log('|        UNIFEI                       |');
+console.log('---------------------------------------');
