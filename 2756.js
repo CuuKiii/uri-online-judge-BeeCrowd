9c8c3928f9a48789d4146b41f24d69103d2fa924
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:19:24 -0300

    Create 2756.js

---
+++ 2756.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('       A');
+console.log('      B B');
+console.log('     C   C');
+console.log('    D     D');
+console.log('   E       E');
+console.log('    D     D');
+console.log('     C   C');
+console.log('      B B');
+console.log('       A');
