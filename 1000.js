c735f771f3da44004aaf3ea55b54a4624e11b4ad
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 12:45:06 -0300

    Create 1000.js

---
+++ 1000.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+console.log ("Hello World!")
