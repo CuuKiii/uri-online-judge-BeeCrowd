bc2c48205d4da5413e9d636d34a1fa16df507244
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:29:26 -0300

    Create 2754.js

---
+++ 2754.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('234.345000 - 45.698000');
+console.log('234 - 46');
+console.log('234.3 - 45.7');
+console.log('234.34 - 45.70');
+console.log('234.345 - 45.698');
+console.log('2.343450e+02 - 4.569800e+01');
+console.log('2.343450E+02 - 4.569800E+01');
+console.log('234.345 - 45.698');
+console.log('234.345 - 45.698');
