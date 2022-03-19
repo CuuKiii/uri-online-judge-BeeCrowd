d31131ce18363a2989c9b8587d08dd921852d98f
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:18:47 -0300

    Create 2752.js

---
+++ 2752.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('<AMO FAZER EXERCICIO NO URI>');
+console.log('<    AMO FAZER EXERCICIO NO URI>');
+console.log('<AMO FAZER EXERCICIO >');
+console.log('<AMO FAZER EXERCICIO NO URI>');
+console.log('<AMO FAZER EXERCICIO NO URI    >');
+console.log('<AMO FAZER EXERCICIO NO URI>');
+console.log('<          AMO FAZER EXERCICIO >');
+console.log('<AMO FAZER EXERCICIO           >');
