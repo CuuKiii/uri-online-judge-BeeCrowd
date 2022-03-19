aa3d2e9c83e69761f1f5ef88103995ef04ddf0cb
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:22:36 -0300

    Create 2747.js

---
+++ 2747.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('---------------------------------------');
+console.log('|                                     |');
+console.log('|                                     |');
+console.log('|                                     |');
+console.log('|                                     |');
+console.log('|                                     |');
+console.log('---------------------------------------');
