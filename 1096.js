144c786fe6696d5a7e43cc221bbe590ba8adf403
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-03-19 13:15:59 -0300

    Create 1096.js

---
+++ 1096.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log('I=1 J=7');
+console.log('I=1 J=6');
+console.log('I=1 J=5');
+console.log('I=3 J=7');
+console.log('I=3 J=6');
+console.log('I=3 J=5');
+console.log('I=5 J=7');
+console.log('I=5 J=6');
+console.log('I=5 J=5');
+console.log('I=7 J=7');
+console.log('I=7 J=6');
+console.log('I=7 J=5');
+console.log('I=9 J=7');
+console.log('I=9 J=6');
+console.log('I=9 J=5');
