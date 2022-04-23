139936839bdc06107c2f576a147436bef8ad15c3
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-04-23 18:40:11 -0300

    Create 3055.js

---
+++ 3055.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const A = parseInt(lines.shift());
+const B = parseInt(lines.shift());
+
+console.log(`${B * 2 - A}`);
