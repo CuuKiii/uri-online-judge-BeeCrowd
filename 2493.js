0ffb63cb5113a8bc77d3151b000b0d1ef100cec9
CuuKiii <69162528+CuuKiii@users.noreply.github.com>
2022-05-03 20:08:26 -0300

    Update 2493.js

--- 2493.js
+++ 2493.js
@@ -1,4 +1,3 @@
-
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 const values = input.split("\n");
@@ -10,6 +9,7 @@ const operacoes = (loop) => {
     const listaq = formatacoes.map((q) =>
       parseInt(q)
     );
+
     if (listaq[0] + listaq[1] === listaq[2]) {
       respostas.push("+");
     } else if (listaq[0] - listaq[1] === listaq[2]) {
@@ -22,28 +22,36 @@ const operacoes = (loop) => {
   }
   return respostas;
 };
+
 const jogadores = (loop, listAnwser) => {
   const errou = [];
   for (let i = 0; i < loop; i++) {
     const [name, question, answer] = values.shift().split(" ");
+
     if (answer !== listAnwser[question - 1]) {
       errou.push(name);
     }
   }
+
   if (errou.length === 0) {
     return "You Shall All Pass!";
   }
+
   if (errou.length === loop) {
     return `None Shall Pass!`;
   }
+
   errou.sort();
   return errou.join(" ");
 };
+
 while (values.length > 0) {
   const loop = parseInt(values.shift());
+
   if (isNaN(loop)) {
     break;
   }
+
   const listQuestionsAnwser = operacoes(loop);
   console.log(jogadores(loop, listQuestionsAnwser));
 }
