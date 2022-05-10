45a5b3d1065c628a3d08d7cd89c5e73899cfd264
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2022-05-09 23:13:02 -0300

    Update 2373.js

--- 2373.js
+++ 2373.js
@@ -9,3 +9,22 @@ for(let i = 1; i <= n; i += 1) {
 }
 
 console.log(brokenGlasses)
+
+//Outra resolução
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var prompt = function(texto) { return lines.shift();};
+
+const garcion = parseInt(prompt());
+var taca, copo;
+var quebrado = 0;
+
+for (let i = 0; i < garcion; i++) {
+  [taca, copo] = prompt().split(" ").map(Number);
+
+  if (taca > copo) {
+    quebrado += copo;
+  }
+}
+
+console.log(quebrado);
