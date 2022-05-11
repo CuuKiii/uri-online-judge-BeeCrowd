d9f4bed2c451003d6fd37ed8e381ecb58444d2d3
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2022-05-11 06:56:11 -0300

    Update 1172.js

--- 1172.js
+++ 1172.js
@@ -9,3 +9,22 @@ const x = require('fs')
 for (let i = 0; i < 10; i++) {
   console.log(`X[${i}] = ${x[i]}`);
 }
+
+
+
+//outra resolucao
+var prompt = function(texto) { return lines.shift();};
+
+const arr = [];
+var num;
+
+for (let i = 0; i < 10; i++) {
+  num = parseInt(prompt());
+  arr.push(num);
+
+  if (arr[i] < 1) {
+    arr[i] = 1;
+  }
+
+  console.log(`X[${i}] = ${arr[i]}`);
+}
