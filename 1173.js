70a25e604ddf5bb31e4fd57f547ffc720a06d500
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2022-05-11 07:11:25 -0300

    Update 1173.js

--- 1173.js
+++ 1173.js
@@ -7,3 +7,16 @@ for (let i = 1; i < 10; i++) {
   n.push(n[i - 1] * 2);
   console.log(`N[${i}] = ${n[i]}`);
 }
+//outra resolução 
+//var prompt = function(texto) { return lines.shift();};
+
+const num = parseInt(prompt());
+const arr = [num];
+
+for (let i = 0; i < 9; i++) {
+  arr.push(arr[i] * 2);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(`N[${i}] = ${arr[i]}`);
+}
