03e19ddf0c01925bf3360ce4f70aa4499f188af7
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2022-05-19 00:27:14 -0300

    Create 2926.js

---
+++ 2926.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+const n = Number(lines[0]);
+
+console.log(`Ent${'a'.repeat(n)}o eh N${'a'.repeat(n)}t${'a'.repeat(n)}l!`);
