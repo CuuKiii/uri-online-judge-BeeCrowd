3ddf54c32f9df1511e3b144e926e5d55a05a3d22
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2022-08-06 20:42:11 -0300

    Create 2749.js

---
+++ 2749.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+console.log(`---------------------------------------
+|x = 35                               |
+|                                     |
+|               x = 35                |
+|                                     |
+|                               x = 35|
+---------------------------------------`)
