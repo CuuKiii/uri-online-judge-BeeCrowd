c780254c61c05b02b94d57900268d817d4de7be1
Leonardo Morais <69162528+CuuKiii@users.noreply.github.com>
2022-08-06 20:47:35 -0300

    Create 2374.js

---
+++ 2374.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var prompt = function(texto) { return lines.shift(); };
+
+var N = parseInt(prompt(""))
+var M = parseInt(prompt(""))
+
+console.log(N - M)
